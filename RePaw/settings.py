8bac0778aa563c28ee1698c1ffe697107a17bc8e
Panagiotis Efstathiadis <visualbasic008@gmail.com>
2024-06-10 21:46:43 +0300

    Serializer

--- RePaw/settings.py
+++ RePaw/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+g(9!6p!(a1%fz457zt86bp&&$a6%aw@zzgvrwrj006nrvx=w$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
